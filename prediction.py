285b3d81a72b6ef28dad74749a301f852eb8c46c
sehhong318 <sehhong318@gmail.com>
2023-06-13 00:18:04 +0800

    add large

--- prediction.py
+++ prediction.py
@@ -1,8 +1,19 @@
 import joblib
 def predict(data):
 
-    # clf = joblib.load(“rf_model.sav”) 
+    rlf = joblib.load('./Model/rf_regression.sav')
+    clf = joblib.load('./Model/xgb_classification.sav')
 
-    # return clf.predict(data)
+    reg_result = rlf.predict(data)
 
-    return data
+    class_result = clf.predict(data)
+    class_label = ''
+
+    if class_result == 0:
+        class_label = 'Low'
+    elif class_result == 1:
+        class_label = 'Medium'
+    else:
+        class_label = 'High'
+
+    return reg_result, class_label
